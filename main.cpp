5228cf27f8632987a1404fc63f497df95382edf9
K. L. Shashin Kalpajith <99177323+IT21355196@users.noreply.github.com>
2022-05-20 22:07:52 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,64 @@
 #include <iostream>
 #include "Report.h"
+#include "GuestUser.h"
+#include "RegisteredUser.h"
+#include "Payment.h"
+#include "Booking.h"
+#include "Ticket.h"
+#include "Bus.h"
+#include "Driver.h"
+#includde "BusSchedule.h"
+#include "Feedback.h"
 
 using namespace std;
 
 int main()
 {
+  
+  // Creating Dynamic Objects
+  
+  GuestUser *gu1;
+  gu1 = new GuestUser(); // Dynamic Object for GuestUser
+  
+  RegisteredUser *ru1;
+  ru1 = new RegisteredUser(); // Dynamic Object for RegisteredUser
+  
+  Booking * b1;
+  b1 = new Booking(); // Dynamic Object for Booking
+  
+  Payment = p1;
+  p1 = new Payment(); // Dynamic Object for Payment
+  
+  BusSchedule = bs1;
+  bs1 = new BusSchedule(); // Dynamic Object for BusSchedule
+  
+  Bus = bus1;
+  bus1 = new Bus(); // Dynamic Object for Bus
+  
+  Driver = d1;
+  d1 = new Driver(); // Dynamic Object for Driver
+  
+  Ticket = t1;
+  t1 = new Ticket(); // Dynamic Object for Ticket
+  
+  Feedback = f1;
+  f1 = new Feedback(); // Dynamic Object for Feedback
+  
+  Report = r1;
+  r1 = new Report(); // Dynamic Object for Report
+  
+  //deallocate Memory (Release Memory)
+  
+  delete gu1;
+  delete ru1;
+  delete b1;
+  delete p1;
+  delete bs1;
+  delete bus1;
+  delete d1;
+  delete t1;
+  delete f1; 
+  delete r1;
   
   return 0;
 }
